0471f0affec22c867ff4ace7bb5c4f5714a515d4
sriinithareddyc <64062185+sriinithareddyc@users.noreply.github.com>
2021-08-03 09:45:28 +0530

    Update RenIoT.js
    Changes made to CSS

--- static/RenIoT.js
+++ static/RenIoT.js
@@ -1,7 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, TextInput, ImageBackground,Switch } from 'react-native';
-
+import { Image} from 'react-native';
+import { withTheme } from 'styled-components';
 class RenIoT extends Component {
   state = {
     req_temp: '',
@@ -13,23 +14,33 @@ class RenIoT extends Component {
     return (
       <ImageBackground
         style={{ width: null, height: null, flex: 1 }}
-        source={require('../RenIoT/assets/renIoTBackground.jpg')}>
+        source={require('../RenIoT/assets/renIoTBackground.png')}>
         <View style={styles.container}>
+        <Image
+        source={require('../RenIoT/assets/person-icon.png')}
+        fadeDuration={0}
+        style={{ width: 50, height: 50 }}
+      />
           <Text>
             <Text style={styles.header}>RenIoT</Text>
             <Text style={[styles.header, styles.sub_header]}> Box</Text>
           </Text>
           <View style={{ marginTop: 20, justifyContent: 'center' }}>
             <Text>
-              <Text style={styles.sub_header}>Battery Percentage </Text>
-              <Text style={styles.sub_header}> 55%</Text>
+              <Text style={styles.sub_header}>Battery Percentage    </Text>
+              <Text style={styles.underline}>55%</Text>
             </Text>
+            <Text>
+              <Text style={styles.sub_header}> Lid OFF    </Text>
             <Switch
           trackColor={{false: 'gray', true: 'teal'}}
           thumbColor="white"
+          backgroundColor="gray"
           onValueChange={(value) => this.setState({toggle: value})}
           value={this.state.toggle}
         />
+        <Text style={styles.sub_header}>    Lid ON </Text>
+        </Text>
             <Text style={styles.sub_header}>Box Temperature</Text>
             <Text style={styles.sub_header}>7&#176;C</Text>
             <Text style={styles.sub_header}>Required Temperature:</Text>
@@ -39,8 +50,21 @@ class RenIoT extends Component {
               placeholderTextColor="#9a73ef"
               autoCapitalize="none"
               onChangeText={this.handleChangeTemp} />
-            <Text style={styles.sub_header}>Estimated Time</Text>
-            <Text style={styles.sub_header}>7 mins</Text>
+              <Text>
+            <Text style={styles.sub_header}>Estimated Time    </Text>
+            <Text style={styles.underline}>7 mins</Text>
+            </Text>
+            <View style={styles}>
+      <Image
+
+        source={require('../RenIoT/assets/danger-icon.jpg')}
+        style={{ width: 50, height: 50,left:70,top:50}}
+      />
+      <Image
+        source={require('../RenIoT/assets/secure-icon.jpg')}
+        style={{ width: 50, height: 50,left:150}}
+      />
+      </View>
           </View>
         </View>
       </ImageBackground>
@@ -52,26 +76,42 @@ const styles = StyleSheet.create({
   container: {
     alignItems: 'center',
     justifyContent: 'center',
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: '#FFFFFF',
+    position:'absolute',
+    alignSelf:'center',
+    textAlign: 'center',
+    width:400,
+    padding:10,
   },
   header: {
     fontSize: 50,
     color: 'red',
+    lineHeight:30,
   },
   sub_header: {
-    color: 'black',
-    fontSize: 25,
+    color: 'white',
+    fontSize: 23,
+    lineHeight:40,
   },
-  image: {
+  Image: {
     flex: 1,
     resizeMode: "cover",
     justifyContent: "center",
     width: '100%',
-    height: '100%'
+    height: '100%',
+    lineHeight:40,
+  },
+  underline:{
+    textDecorationLine: 'underline',
+    color:'white',
   },
   textInput: {
     borderColor: 'gray',
     borderWidth: 1,
     textAlign: 'center',
+    lineHeight:40,
   }
 });
 
